cef39bc79e8c7c8a66b4ddc9402e2ef91ae0eb54
craftqq <nimdrop@t-online.de>
2016-02-13 15:04:39 +0100

    comments on what to do in Main

--- Solar_Consortium/src/Main.java
+++ Solar_Consortium/src/Main.java
@@ -1,10 +1,52 @@
+import io.github.craftqq.GUI.Fenster;
 
+/**
+ * the main class, everything starts here, and everything should end here.
+ * @author Martin
+ *
+ */
 public class Main 
 {
-
+	//GUI
+	private Fenster fenster;
+	//DATA
+	//file access
+	//state while running
+	
+	//ENGINE
+	//calculating stuff
+	//server / client stuff (maybe to data?)
+	
+	/**
+	 * the main method.
+	 * TODO: implement stuff;
+	 * @param args
+	 */
 	public static void main(String[] args) 
+	{
+		setup();
+		//game running, put stuff here
+		
+		destruct();
+		System.exit(0);
+	}
+	
+	/**
+	 * Everything that has to be done before the user should be able to do something;
+	 * TODO: implement stuff, maybe add arguments / error code or something to determine if it worked.
+	 */
+	private static void setup()
 	{
 		
 	}
 
+	/**
+	 * stuff we should do before the program exits.
+	 * TODO:implement stuff, arguments? / error code?
+	 */
+	public static void destruct()
+	{
+		
+	}
+	
 }
